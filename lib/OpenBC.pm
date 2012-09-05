f53a4858c96dfb9a49c1e6b63ebeadaa124f9e4d
Brad Roger <bradroger@gmail.com>
2012-09-05 10:17:35 -0700

    Basic working admin backend

--- lib/OpenBC.pm
+++ lib/OpenBC.pm
@@ -2,24 +2,53 @@ package OpenBC;
 use Dancer;
 use OpenBC::wiki;
 
+hook 'before' => sub {
+    if (!session('user') && request->path_info =~ m{^/edit} && request->path_info =~ m{^/admin} && request->path_info ne '/admin/sign_out' && request->path_info ne 'admin/login') {
+        request->path_info('/admin/login');
+    }
+};
+
+hook 'before_template_render' => sub {
+    my $tokens = shift;
+    $tokens->{username} = session('user');
+};
+
 get '/' => sub {
     return 'Hello World!!';
 };
 
 get '/view' => sub {
-    template 'view.tt', {};
+#    template 'view.tt', {};
+    return 'View what?';
+};
+
+get '/view/:file' => sub {
+    my $self = shift;
+    my $wiki = OpenBC::wiki->new;
+    my $file = params->{file};
+    my $toc = $wiki->read($file.":toc");
+
+    template 'view.tt', { file => $file, toc => $toc, content => $wiki->read($file) };
 };
 
 get '/admin/sign_out' => sub {
-    session user => '';
     session->destroy;
-    return "Signed Out";
+    redirect '/admin';
 };
 
-get '/admin/*' => sub {
+get '/admin/login' => sub {
+    my $self = shift;
+    my $env = request ->env();
+    my $doorman = $env->{'doorman.admin.twitter'};
+    my $content = "Please <a href=\"@{[ $doorman->sign_in_path ]}\">Login with Twitter</a>";
+    template 'admin.tt', { content => $content };
+};
+
+get '/admin' => sub {
     my $self = shift;
     my $env = request->env(); 
     my $doorman = $env->{'doorman.admin.twitter'};
+    my $wiki = OpenBC::wiki->new;
     my $out;
 
     # Check sign-in status, and provide sign-out link or sign-in link in the output.
@@ -28,19 +57,70 @@ get '/admin/*' => sub {
         session user => $doorman->twitter_screen_name;
     }
     else {
-        $out = "<a href=\"@{[ $doorman->sign_in_path ]}\">Login</a>";
+        redirect '/admin/login';
     }
-    return $out;
+    if ( session('user') eq "bradroger") {
+        $out = $out."<br><br><br><div class='span4'><div class='well'><h1>Codes</h1>";
+        $out = $out.$wiki->list;
+        $out = $out."<br><a href='edit/new/' class='btn'>Add a new code</a></div></div>";
+    }
+    template 'admin.tt', { content => $out, username => session('user') }; 
+};
+
+get '/admin/*' => sub {
+    redirect '/admin';
+};
+
+get '/edit/new/' => sub {
+    if ( session('user') ne "bradroger") { warn session('user'); redirect 'admin'; }
+    template 'new.tt', {};
+};
+
+get '/edit/new/:file' => sub {
+    if ( session('user') ne "bradroger") { warn session('user'); redirect 'admin'; }
+    template 'new.tt', { file => params->{file} };
+};
+
+post '/edit/new/' => sub {
+    my $wiki = OpenBC::wiki->new;
+    my $file = param "codename";
+    $wiki->add($file);
+    $wiki->write($file.':txturl', param "txturl");
+    $wiki->write($file.':pdfurl', param "pdfurl");
+    $wiki->write($file.':codetype', param "codetype");
+    $wiki->write($file.':location', param "location");
+    $wiki->write($file.':date', param "date");
+#    $wiki->write($file.':codeid', param "codeid");
+#    $wiki->write($file.':tocid', param "tocid");
+    redirect '/edit/'. $file;
+};
+
+get '/edit/delete/:file' => sub {
+    my $wiki = OpenBC::wiki->new;
+    $wiki->delete(params->{file});
+    redirect '/admin';
 };
 
 get '/edit/:file' => sub {
-    if ( session('user') ne "bradroger") { warn session('user'); redirect 'admin/start'; }
+    if ( session('user') ne "bradroger") { warn session('user'); redirect 'admin'; }
     my $wiki = OpenBC::wiki->new;
-    my $string = $wiki->read(params->{file});
-    template 'edit.tt', { file => $string };
+    my $file = $wiki->read(params->{file});
+    my $title = params->{file};
+    my $txturl = $wiki->read(params->{file}.':txturl');
+    my $pdfurl = $wiki->read(params->{file}.':pdfurl');
+    my $codetype = $wiki->read(params->{file}.':codetype');
+    my $location = $wiki->read(params->{file}.':location');
+    my $date = $wiki->read(params->{file}.':date');
+    my $tocid = $wiki->read(params->{file}.':tocid');
+    my $codeid = $wiki->read(params->{file}.':codeid');
+
+    my $revisions = $wiki->listrevisions( params->{file} );
+    
+    template 'edit.tt', { file => $file, title => $title, txturl => $txturl, pdfurl => $pdfurl, codetype => $codetype, location => $location, date => $date, tocid => $tocid, codeid => $codeid, revisions => $revisions };
 };
 
 post '/edit/:file' => sub {
+    if ( session('user') ne "bradroger") { warn session('user'); redirect 'admin'; }
     my $wiki = OpenBC::wiki->new;
     $wiki->write(params->{file}, param "content");
     redirect '/edit/'.params->{file};
--- lib/OpenBC/wiki.pm
+++ lib/OpenBC/wiki.pm
@@ -1,6 +1,7 @@
 package OpenBC::wiki;
 use Moose;
 use Redis;
+use Time::Stamp -stamps => { dt_sep => ' ', date_sep => '.', us => 1 };
 
 has 'db' => (is => 'ro', lazy_build => 1);
 
@@ -13,15 +14,55 @@ sub write {
     my $self = shift;
     my $key = shift;
     my $value = shift;
+    if ($self->db->get($key)) {
+        my $backup = $self->db->get($key);
+        my $backupkey = $key."_".localstamp();
+        $self->db->set( $backupkey => $backup );
+        $self->db->lpush($key.":backup",$backupkey );
+    }
     $self->db->set( $key => $value );
     return;
 }
 
-sub list {
-
-
+sub listrevisions {
+    my $self = shift;
+    my $key = shift;
+    my $len = $self->db->llen($key.":backup");
+    my $out = "<ul>";
+    for (my $i=0; $i<$len; $i++) {
+        my $code = $self->db->lpop($key.":backup");
+        $out = $out."<li><a href=\"../view/".$code."\">".$code."</a></li>";
+        $self->db->rpush($key.':backup',$code);
+    }
+    $out = $out."</ul>";
+    return $out;
 }
 
+sub list {
+    my $self = shift;
+    my $len = $self->db->llen('codelist');
+    my $out = "<ul>";
+    for (my $i=0; $i<$len; $i++) {
+        my $code = $self->db->lpop('codelist');
+        $out = $out."<li><a href=\"view/".$code."\">".$code."</a> <a class=\"btn btn-primary btn-mini\" href=\"edit/".$code."\">Edit</a></span></li>";
+        $self->db->rpush('codelist',$code);
+    }
+    $out = $out."</ul>";
+    return $out;
+}
+
+sub add {
+    my $self = shift;
+    my $file = shift;
+    $self->db->lpush('codelist',$file );
+}
+
+sub delete {
+    my $self = shift;
+    my $file = shift;
+    $self->db->lrem('codelist',0,$file);
+    $self->db->del($file);
+}
 sub _build_db { Redis->new }
 
 1;
